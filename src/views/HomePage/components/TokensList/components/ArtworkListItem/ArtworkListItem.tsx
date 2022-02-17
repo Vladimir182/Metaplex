b346ca181e7796336726b2136e295b9b8b73f829
Irina Kadushkina <69786983+ikadushkina@users.noreply.github.com>
2022-02-17 16:54:04 +0300

    feat: added market_id for token card (#9)

--- src/views/HomePage/components/TokensList/components/ArtworkListItem/ArtworkListItem.tsx
+++ src/views/HomePage/components/TokensList/components/ArtworkListItem/ArtworkListItem.tsx
@@ -1,4 +1,4 @@
-import { HStack, StackDivider } from "@chakra-ui/react";
+import { HStack, StackDivider, Text, Flex } from "@chakra-ui/react";
 import React from "react";
 import { MarketState } from "@metaplex-foundation/mpl-fixed-price-sale/dist/src/types";
 
@@ -15,7 +15,13 @@ interface Props {
 }
 
 export const ArtworkListItem: React.FC<Props> = ({ artwork, variant }) => {
-  const { image, title, endDate, state = MarketState.Uninitialized } = artwork;
+  const {
+    image,
+    title,
+    market,
+    endDate,
+    state = MarketState.Uninitialized,
+  } = artwork;
 
   const type = artwork.type;
   const isExhaustedMints = artwork.prints?.supply === artwork.prints?.maxSupply;
@@ -23,28 +29,30 @@ export const ArtworkListItem: React.FC<Props> = ({ artwork, variant }) => {
 
   return (
     <HStack bg="gray.800" spacing={4} p={4} borderRadius="xl" mb={4}>
-      <HStack
-        spacing={4}
-        align="middle"
-        w="calc(100% - 140px)"
-        divider={<StackDivider borderColor="whiteAlpha.100" />}
-      >
-        <ArtworkListItemHeader imgUrl={image} name={title} type={type} />
-        <ArtworkStats
-          supply={artwork.prints?.supply}
-          maxSupply={artwork.prints?.maxSupply}
-          variant={variant}
-        />
-
-        {hasAmount && (
-          <ArtworkListItemSaleAmount
-            title="Primary Sale"
-            saleAmount={artwork.primarySaleAmount}
+      <Flex direction="column" w="calc(100% - 140px)">
+        <HStack
+          spacing={4}
+          align="middle"
+          divider={<StackDivider borderColor="whiteAlpha.100" />}
+        >
+          <ArtworkListItemHeader imgUrl={image} name={title} type={type} />
+          <ArtworkStats
+            supply={artwork.prints?.supply}
+            maxSupply={artwork.prints?.maxSupply}
+            variant={variant}
           />
-        )}
 
-        <ArtworkListItemStatus state={state} />
-      </HStack>
+          {hasAmount && (
+            <ArtworkListItemSaleAmount
+              title="Primary Sale"
+              saleAmount={artwork.primarySaleAmount}
+            />
+          )}
+
+          <ArtworkListItemStatus state={state} />
+        </HStack>
+        <Text fontSize={14}>{market}</Text>
+      </Flex>
       <ArtworkListItemActions
         artwork={artwork}
         state={state}
